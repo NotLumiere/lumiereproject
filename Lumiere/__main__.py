638d1a2334f4fe94dc809d182e9d3812df9a1e7a
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2024-07-13 12:48:31 +0700

    Update __main__.py

--- Lumiere/__main__.py
+++ Lumiere/__main__.py
@@ -19,9 +19,9 @@ from traceback import format_exc
 from telethon import version
 from telethon.tl.alltlobjects import LAYER
 
-from AyiinXd import Ayiin, LOGS, LOOP, bot
-from AyiinXd.ayiin import HOSTED_ON, autobot, autopilot, checking, heroku
-from AyiinXd.modules import ALL_MODULES
+from Lumiere import Lumi, LOGS, LOOP, bot
+from Lumiere.lumi import HOSTED_ON, autobot, autopilot, checking, heroku
+from Lumiere.modules import ALL_MODULES
 
 
 ON = '''
@@ -36,25 +36,25 @@ ON = '''
 '''
 
 
-async def AyiinMain():
+async def LumiMain():
     from config import var
 
-    await Ayiin.start()
+    await Lumi.start()
     if not var.BOTLOG_CHATID:
         await autopilot()
     if not var.BOT_TOKEN:
         await autobot()
     try:
         for module_name in ALL_MODULES:
-            imported_module = import_module(f"AyiinXd.modules.{module_name}")
+            imported_module = import_module(f"Lumiere.modules.{module_name}")
         LOGS.info(f"Python Version - {python_version()}")
         LOGS.info(f"Telethon Version - {version.__version__} [Layer: {LAYER}]")
         LOGS.info(f"Userbot Version - {var.BOT_VER}")
         LOGS.info("[✨ BERHASIL DIAKTIFKAN! ✨]")
-        await checking(Ayiin)
-        me = await Ayiin.get_me()
+        await checking(Lumi)
+        me = await Lumi.get_me()
         bo = await bot.get_me()
-        await Ayiin.send_message(var.BOTLOG_CHATID, ON.format(var.BOT_VER, HOSTED_ON, me.id, me.first_name, bo.id, bo.first_name))
+        await Lumi.send_message(var.BOTLOG_CHATID, ON.format(var.BOT_VER, HOSTED_ON, me.id, me.first_name, bo.id, bo.first_name))
     except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
         pass
     except BaseException as e:
@@ -65,15 +65,15 @@ async def AyiinMain():
 if __name__ == "__main__":
     try:
         heroku()
-        LOOP.run_until_complete(AyiinMain())
+        LOOP.run_until_complete(LumiMain())
     except BaseException:
         LOGS.error(format_exc())
         sys.exit()
 
 if len(sys.argv) not in (1, 3, 4):
-    Ayiin.disconnect()
+    Lumi.disconnect()
 else:
     try:
-        Ayiin.run_until_disconnected()
+        Lumi.run_until_disconnected()
     except ConnectionError:
         pass
