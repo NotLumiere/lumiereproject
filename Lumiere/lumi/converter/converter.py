8589e02151404472eb2dbb57f0cc9bf4b3219a8b
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2024-07-13 13:47:49 +0700

    Update converter.py

--- Lumiere/lumi/converter/converter.py
+++ Lumiere/lumi/converter/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 from os import path
 
-from AyiinXd.ayiin.exceptions import FFmpegReturnCodeError
+from Lumiere.lumi.exceptions import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
