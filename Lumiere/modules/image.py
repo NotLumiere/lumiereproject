72b154f8bc607773d61b66db2e0d17efec23351a
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2024-07-15 12:38:26 +0700

    Update and rename yinsimg.py to image.py

--- Lumiere/modules/image.py
+++ Lumiere/modules/image.py
@@ -475,7 +475,7 @@ async def pixelator(event):
 
 CMD_HELP.update(
     {
-        "yinsimg": f"**Plugin : **`yinsimg`\
+        "img": f"**Plugin : **`image`\
         \n\n  »  **Perintah :** `{cmd}sketch` <reply ke Foto/Sticker>\
         \n  »  **Kegunaan :** Coba Dulu Tod.\
         \n\n  »  **Perintah :** `{cmd}grey` <reply ke Foto/Sticker>\
