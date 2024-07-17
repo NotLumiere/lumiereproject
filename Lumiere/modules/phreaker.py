1cf50e2d1b8d5f18ff1f8fba292d079d5da3a029
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2024-07-17 14:29:08 +0700

    Update phreaker.py

--- Lumiere/modules/phreaker.py
+++ Lumiere/modules/phreaker.py
@@ -2,8 +2,8 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest
 
-from AyiinXd import CMD_HELP
-from AyiinXd.ayiin import edit_or_reply, ayiin_cmd
+from Lumiere import CMD_HELP
+from Lumiere.lumi import edit_or_reply, ayiin_cmd
 
 from . import cmd
 
