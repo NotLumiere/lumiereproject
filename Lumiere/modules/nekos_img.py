8db538b3a34af34c40f60b394fb3f256efdccd1d
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2024-07-23 02:44:53 +0700

    Update nekos_img.py

--- Lumiere/modules/nekos_img.py
+++ Lumiere/modules/nekos_img.py
@@ -6,8 +6,8 @@
 
 import nekos
 
-from AyiinXd import CMD_HELP
-from AyiinXd.ayiin import ayiin_cmd, eod, eor
+from Lumiere import CMD_HELP
+from Lumiere.lumi import ayiin_cmd, eod, eor
 
 from . import cmd
 
